5fdb54c0f2abaf93f280c855600dfcd7e9aab050
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-02-06 10:10:54 +0530

    added styles for page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,9 +1,41 @@
-import React from 'react'
+import { Spotlight } from "@/components/ui/Spotlight";
+import React from "react";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5 h-screen">
+      <div className="pb-20 pt-36 w-full">
+        <div>
+          <Spotlight
+            className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+            fill="white"
+          />
+          <Spotlight
+            className="h-[80vh] w-[50vw] top-10 left-full"
+            fill="purple"
+          />
+          <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
+        </div>
+        <div
+          className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+        >
+          {/* Radial gradient for the container to give a faded look */}
+          <div
+            // chnage the bg to bg-black-100, so it matches the bg color and will blend in
+            className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+          />
+        </div>
+        <div className="flex justify-center relative my-10 z-10">
+          <div className="flex justify-start items-center gap-4">
+            <h2 className="text-2xl font-semibold p-0 m-0">About Me</h2>
+            <div className="w-[200px] h-[1px] bg-gray-400"></div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
 
-export default About
+export default About;
