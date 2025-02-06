c3214c5e9daa7c7f9f23836c59255df5651f1f15
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-02-06 10:46:31 +0530

    added styles to navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,29 +1,44 @@
 import Link from "next/link";
 import React from "react";
+import '../style/navbar.css'
 
 const Navbar = () => {
   return (
-    <div>
-      <ul className="cs_nav_list fw-medium flex justify-center items-center gap-4">
-        <li className="menu-item-has-children">
-          <Link href="/">Home</Link>
-        </li>
+    <nav className="fixed top-0 left-0 w-full bg-black-100/80 backdrop-blur-lg shadow-md z-50">
+      <div className="max-w-7xl mx-auto px-6 md:px-12 lg:px-20">
+        <ul className="flex justify-center items-center gap-8 py-4 text-white text-lg">
+          <li className="relative group">
+            <Link href="/" className="nav-link">
+              Home
+            </Link>
+          </li>
 
-        <li className="menu-item-has-children">
-          <Link href="/projects">Projects</Link>
-        </li>
+          <li className="relative group">
+            <Link href="/projects" className="nav-link">
+              Projects
+            </Link>
+          </li>
 
-        <li>
-          <Link href="/about">About</Link>
-        </li>
-        <li>
-          <Link href="/blog">Blog</Link>
-        </li>
-        <li>
-          <Link href="/contact">Contact</Link>
-        </li>
-      </ul>
-    </div>
+          <li className="relative group">
+            <Link href="/about" className="nav-link">
+              About
+            </Link>
+          </li>
+
+          <li className="relative group">
+            <Link href="/blog" className="nav-link">
+              Blog
+            </Link>
+          </li>
+
+          <li className="relative group">
+            <Link href="/contact" className="nav-link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </nav>
   );
 };
 
