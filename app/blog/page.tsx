7db1b3991b0d3146193d5d578fe706f10ecf347e
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-11 15:51:19 +0530

    fixed the issue of container hight

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -17,17 +17,14 @@ const Blog = () => {
           />
           <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
         </div>
-        <div
-          className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
-                   absolute top-0 left-0 flex items-center justify-center"
-        >
+        <div className="w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2] relative">
           {/* Radial gradient for the container to give a faded look */}
           <div
             // chnage the bg to bg-black-100, so it matches the bg color and will blend in
             className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
                      bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
           />
-          <div className="md:mt-[600px] md:w-full w-[90%] mt-[1024px]">
+          <div className="w-full max-w-5xl mx-auto md:px-4">
             <React19Post />
           </div>
         </div>
