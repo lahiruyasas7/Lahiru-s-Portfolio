1834d9968f84ff1030d8e88e5d5056ed30c1e65f
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-06-12 11:15:18 +0530

    feat: changed the images oder of online compiler project

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -27,8 +27,8 @@ export const projects: Project[] = [
     description:
       "An online compiler with error showing for c, c++, java, python, html. use technologies are react js, monaco editor, bootstrap, react-select",
     images: [
-      "custom-images/onlineCompiler1.PNG",
       "custom-images/onlineCompilerProject2.PNG",
+      "custom-images/onlineCompiler1.PNG",
     ],
   },
   {
