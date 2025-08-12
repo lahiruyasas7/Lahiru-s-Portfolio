5dc581fe7acf7da577271199f39ef87d3bc45e70
Lahiru Yasas <lahiruyasas7@gmail.com>
2025-08-12 20:38:24 +0530

    feat: added latest cv

--- app/resume/page.tsx
+++ app/resume/page.tsx
@@ -30,7 +30,7 @@ const Resume = () => {
         </div>
         <div className="relative my-5 z-10 xl:mx-[200px]">
           <iframe
-            src="/custom-images/Lahiru Karunathilaka - Resume.pdf"
+            src="/custom-images/Lahiru Karunathilaka (Full-Stack SE) - Resume.pdf"
             width="100%"
             height="800px"
           />
